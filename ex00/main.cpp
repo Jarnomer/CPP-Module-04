e0514a3340a31c29c50c952bb8ff5871d287fbb3
jankku <jankkume@pm.me>
2024-09-27 16:57:27 +0300

    Updated ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -2,13 +2,16 @@
 #include "Cat.hpp"
 #include "Dog.hpp"
 
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
+
 int main(void) {
   const Animal *meta = new Animal();
   const Animal *dog = new Dog();
   const Animal *cat = new Cat();
   std::cout << "\n";
-  std::cout << dog->getType() << " " << std::endl;
-  std::cout << cat->getType() << " " << std::endl;
+  std::cout << dog->getType() << std::endl;
+  std::cout << cat->getType() << std::endl;
   std::cout << "\n";
   cat->makeSound();
   dog->makeSound();
@@ -17,5 +20,17 @@ int main(void) {
   delete meta;
   delete dog;
   delete cat;
+
+  std::cout << "\n";
+  const WrongAnimal *wmeta = new WrongAnimal();
+  const WrongAnimal *wcat = new WrongCat();
+  std::cout << "\n";
+  std::cout << wcat->getType() << std::endl;
+  std::cout << "\n";
+  wcat->makeSound();
+  wmeta->makeSound();
+  std::cout << "\n";
+  delete wmeta;
+  delete wcat;
   return 0;
 }
